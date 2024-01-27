95ef68b161dbe07210ee692c7935e7c67acd91f1
Shane-Longman <51136041+Shane-Longman@users.noreply.github.com>
2024-01-27 13:54:05 +0100

    Update pairs.py - test binance fix

--- scripts/pairs.py
+++ scripts/pairs.py
@@ -5,6 +5,14 @@ import requests
 import json
 import sys
 
+def http_get_with_urllib3(url):
+    import urllib3
+    http = urllib3.PoolManager()
+    r = http.request('GET', url, preload_content=False)
+    status = r.status
+    data = r.read()
+    r.release_conn()
+    return status, data
 
 def main():
 
@@ -14,16 +22,22 @@ def main():
         print(kraken_res.text, file=sys.stderr)
         return
 
-    bina_res = requests.get('https://www.binance.com/bapi/asset/v2/public/asset-service/product/get-products?includeEtf=false')
-    if bina_res.status_code != 200:
+    #bina_res = requests.get('https://www.binance.com/bapi/asset/v2/public/asset-service/product/get-products?includeEtf=false')
+    #if bina_res.status_code != 200:
+    #    print('[Binance] Bad status code. Aborting', file=sys.stderr)
+    #    print(bina_res.text, file=sys.stderr)
+    #    return
+    bina_status, bina_data = http_get_with_urllib3('https://www.binance.com/bapi/asset/v2/public/asset-service/product/get-products?includeEtf=false')
+    if bina_status != 200:
         print('[Binance] Bad status code. Aborting', file=sys.stderr)
-        print(bina_res.text, file=sys.stderr)
+        print(bina_data, file=sys.stderr)
         return
 
     kraken_json = kraken_res.json()
     kraken_markets = sorted([mkt['wsname'].replace('/', '-') for _, mkt in kraken_json['result'].items()])
 
-    bina_json = bina_res.json()
+#    bina_json = bina_res.json()
+    bina_json = json.loads(bina_data)
     bina_markets = sorted([mkt['b'] + '-' + mkt['q'] for mkt in bina_json['data'] if mkt['st'] == 'TRADING'])
     # clone Binance USDT markets as USDC
     bina_markets.extend(m.replace('-USDT', '-USDC') for m in bina_markets if m.endswith('-USDT'))
